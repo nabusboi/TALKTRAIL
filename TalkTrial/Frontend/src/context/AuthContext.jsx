5bbf848c6ee830f368771d80104e6fdd0153b7ac
nabusboi <138568952+nabusboi@users.noreply.github.com>
2025-04-15 14:23:11 +0530

    Update AuthContext.jsx

--- TalkTrial/Frontend/src/context/AuthContext.jsx
+++ TalkTrial/Frontend/src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+/*import { createContext, useContext, useState } from "react";
 
 export const AuthContext = createContext()
 
@@ -12,3 +12,25 @@ export const AuthContextProvider = ({children}) => {
         {children}
     </AuthContext.Provider>
 }
+*/
+import { createContext, useContext, useState, useMemo } from "react";
+
+export const AuthContext = createContext();
+
+export const useAuthContext = () => {
+    return useContext(AuthContext);
+};
+
+export const AuthContextProvider = ({ children }) => {
+    const [authUser, setAuthUser] = useState(() => {
+        return JSON.parse(localStorage.getItem("auth-user")) || null;
+    });
+
+    const contextValue = useMemo(() => ({ authUser, setAuthUser }), [authUser]);
+
+    return (
+        <AuthContext.Provider value={contextValue}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
